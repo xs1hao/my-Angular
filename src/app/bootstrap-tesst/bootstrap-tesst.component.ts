9ccb67eb17617b81d028900cc0c21e0ebf2f30a2
xs1hao <42058817+xs1hao@users.noreply.github.com>
2020-04-05 12:55:41 +0800

    使用viewchild

--- src/app/bootstrap-tesst/bootstrap-tesst.component.ts
+++ src/app/bootstrap-tesst/bootstrap-tesst.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { ButtonComponent } from '../components/zorro/button/button.component';
 
 @Component({
   selector: 'app-bootstrap-tesst',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bootstrap-tesst.component.less']
 })
 export class BootstrapTesstComponent implements OnInit {
+  objdata:any;
+  str: string;
+
+  @ViewChild('btn', { static: true }) private domLabelChild: ButtonComponent;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.str = "德玛西亚啦啦啦";
+    this.objdata = {
+      name:'sq',
+      id:125
+    }
 
+    this.domLabelChild.methods('使用@viewchild测试第一个值，在父组件中，调用子组件的方法');
+    
+  }
+ 
+  confn(data) {
+    //调用子组件的方法 method2();
+    this.domLabelChild.method2();
+    
+    console.log('子组件给我发送了一个data:',data);
+  }
 }
--- src/app/components/zorro/button/button.component.ts
+++ src/app/components/zorro/button/button.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged, pluck } from 'rxjs/operators';
 
 @Component({
   selector: 'app-button',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
+  @Input() item:String;
+  @Input() data;
+  @Output() con = new EventEmitter<any>();
+
+  @ViewChild('button', { static: true }) private domLabelChild: ElementRef;
+  @ViewChild('myInput', { static: true }) private nzInput: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log('this.item:',this.item);
+    console.log('this.data:',this.data);
+  }
+
+  ngAfterViewInit(): void {
+    /**
+     * DOM节点 ( 主要用来操作dom)
+     * */ 
+    console.log(this.domLabelChild.nativeElement);
+    console.log(this.nzInput.nativeElement);//获取的是DOM 节点  <input _ngcontent-bso-c128="" type="text" placeholder="请输入....">
+
+
+    // 使用viewchild 获取input输入内容.... 
+    // pluck 选择属性来发出
+    fromEvent(this.nzInput.nativeElement, 'input')
+    .pipe(debounceTime(300), distinctUntilChanged(), pluck('target', 'value'))
+    .subscribe((value: string) => {
+        console.log('value:',value);// input中输入的内容
+        // this.onSearch.emit(value);
+    });
+  }
+
+  // 通过viewchild 父组件可以直接调用 子组件的方法 ；
+  methods(val){
+    console.log('值为'+val)
+  }
+  // 改变子组件的值
+  method2(){
+    this.item = '父组件改变了我，王者剑圣归来......'
+  }
+  click () {
+    this.con.emit(this.data);
   }
 
 }
