4098a3c11ddce1a0b73ca59cf5b4296e99d695ea
xs1hao <42058817+xs1hao@users.noreply.github.com>
2020-06-14 16:18:09 +0800

    使用Angular Api属性 NgPlural设置表格模板

--- src/app/pages/angular-api/ng-plural/ng-plural.component.ts
+++ src/app/pages/angular-api/ng-plural/ng-plural.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Constant } from 'src/app/shared/core/utlis/constant';
+
+
 
 @Component({
   selector: 'app-ng-plural',
@@ -9,7 +12,12 @@ export class NgPluralComponent implements OnInit {
 
   category = '';
   options = [];
-  constructor() { }
+  mockData = [];
+  theadConfig = [];
+
+  constructor(
+    private constant: Constant
+  ) { }
 
   ngOnInit(): void {
     this.options = [
@@ -18,11 +26,20 @@ export class NgPluralComponent implements OnInit {
       'workitem',
       'qita'
     ]
+    this.mockData = this.constant.getMockDataWithVision();
+    this.theadConfig = this.constant.getTheadConfigData();
   }
 
   goRelatedApi(eve) {
     this.category = eve;
   }
 
+  toggleClass() {
+    const dot = document.querySelector('#text');
+    dot.classList.add('myClass');
+    setTimeout(()=>{
+      dot.classList.remove('myClass')
+    },1000)
+  }
 
 }
--- src/app/shared/core/utlis/constant.ts
+++ src/app/shared/core/utlis/constant.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class Constant {
-     
+
     getRelateTemplate(key,data){
         const item_name = `<div>
               <span class="text-default">${data.name}</span>
@@ -30,6 +30,86 @@ export class Constant {
             default:
                 break;
         }
+    }
+
+    // 模拟数据
+    getMockDataWithVision(){
+      const mockData = [
+        {
+          id:1,
+          title: '这是一个需求',
+          number: 'RR20200614001',
+          category: '01001001',
+          status: '研发完成',
+          assigned_domain: 'Vison',
+          deadline: '2020年6月14日15',
+          current_owners: [
+            {
+              name: '刘强强',
+              count: '001001'
+            }
+          ]
+        },
+        {
+          id: 2,
+          title: '这是一个问题',
+          number: 'Issue20200614002',
+          category: '01001002',
+          status: '定位中',
+          assigned_domain: 'Fuxi',
+          deadline: '2020年6月13日15',
+          current_owners: [
+            {
+              name: '李欢',
+              count: '001002'
+            }
+          ]
+        },
+        {
+          id: 3,
+          title: '这是一个工作项',
+          number: 'Wi20200614002',
+          category: '01001003',
+          status: '待处理',
+          assigned_domain: 'Titan',
+          deadline: '2020年6月12日15',
+          current_owners: [
+            {
+              name: '史强',
+              count: '001002'
+            }
+          ]
+        }
+      ];
+      return mockData;
+    }
+
+    // 模拟表头数据配置
+    getTheadConfigData() {
+
+      const theadConfig = [
+        {
+          title: '类型',
+          key: 'category',
+          style: {width: '100px', textAlign: 'center'}
+        },
+        {
+          title: '主要信息',
+          key: 'mainInfo',
+          style: {width: '150px', textAlign: 'center'}
+        },
+        {
+          title: '状态',
+          key: 'status',
+          style: {width: '100px', textAlign: 'center'}
+        },
+        {
+          title: '责任人',
+          key: 'current_owners',
+          style: {width: '100px', textAlign: 'center'}
+        }
+      ];
+      return theadConfig;
     }
 
 }
